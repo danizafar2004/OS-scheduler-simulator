a49d44e488037117ade36e9c06ba5cd38d77c3bc
mianali10 <f2023-059@bnu.edu.pk>
2026-01-09 14:40:05 +0500

    updated

--- sjf non prem.cpp
+++ sjf non prem.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     int n;
     cout << "Enter number of processes: ";
     cin >> n;
+
     string pid[20];
     int arrival[20], burst[20];
     int start[20], finish[20], waiting[20], tat[20];
     int done[20] = {0};
+
     for (int i = 0; i < n; i++) {
-        cout << "Enter Process ID: ";
+        cout << "\nEnter Process ID: ";
         cin >> pid[i];
         cout << "Arrival Time: ";
         cin >> arrival[i];
         cout << "Burst Time: ";
         cin >> burst[i];
     }
+
     int completed = 0;
     int time = 0;
-    // this will jump to earliest arrival
+
+    // jump to earliest arrival time
     int minArrival = arrival[0];
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++) {
         if (arrival[i] < minArrival)
             minArrival = arrival[i];
+    }
     time = minArrival;
-    cout << "\nGantt Chart: ";
-    while (completed < n) {
 
+    cout << "\nGantt Chart: ";
+
+    while (completed < n) {
         int idx = -1;
         int bestBurst = 99999;
-        // it will choose the shortest job among arrived processes for
-        (int i = 0; i < n; i++) {
+
+        // choose shortest job among arrived processes
+        for (int i = 0; i < n; i++) {
             if (done[i] == 0 && arrival[i] <= time) {
                 if (burst[i] < bestBurst) {
                     bestBurst = burst[i];
@@ -38,30 +46,39 @@ int main() {
                 }
             }
         }
+
         if (idx == -1) {
             time++;
             continue;
         }
+
         start[idx] = time;
-        time = time + burst[idx];
+        time += burst[idx];
         finish[idx] = time;
         waiting[idx] = start[idx] - arrival[idx];
         tat[idx] = finish[idx] - arrival[idx];
+
         done[idx] = 1;
         completed++;
+
         cout << "| " << pid[idx] << " ";
     }
     cout << "|\n";
+
     double wsum = 0, tsum = 0;
     cout << "\nProcess\tAT\tBT\tWT\tTAT\n";
     for (int i = 0; i < n; i++) {
-        cout << pid[i] << "\t" << arrival[i] << "\t" << burst[i] << "\t" <<
-        waiting[i] << "\t" << tat[i] << endl;
+        cout << pid[i] << "\t"
+             << arrival[i] << "\t"
+             << burst[i] << "\t"
+             << waiting[i] << "\t"
+             << tat[i] << endl;
         wsum += waiting[i];
         tsum += tat[i];
     }
-    cout << "\nAverage Waiting Time: " << wsum / n;
 
+    cout << "\nAverage Waiting Time: " << wsum / n;
     cout << "\nAverage Turnaround Time: " << tsum / n;
+
     return 0;
 }
