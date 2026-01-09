c8f5edea0e0fc70ae555d79a2b5aaa324c1c6f6a
fortx69 <f2023-125@bnu.edu.pk>
2026-01-09 12:30:17 +0500

    Priority Scheduling (non preemp) comitted by MUHAMMAD ZAID

--- priority-scheduling-(non-preemp).cpp
+++ priority-scheduling-(non-preemp).cpp
@@ -1,3 +1,69 @@
-//
-// Created by zaidm on 1/3/2026.
-//
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+    string pid[20];
+    int arrival[20], burst[20], priority[20];
+    int start[20], finish[20], waiting[20], tat[20];
+    int done[20] = {0};
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Process ID: ";
+        cin >> pid[i];
+        cout << "Arrival Time: ";
+        cin >> arrival[i];
+        cout << "Burst Time: ";
+        cin >> burst[i];
+        cout << "Priority (lower = higher): "; cin
+        >> priority[i];
+    }
+    int completed = 0;
+    int time = 0;
+    // jumping to first arrival
+    int minArrival = arrival[0];
+    for (int i = 1; i < n; i++)
+        if (arrival[i] < minArrival)
+            minArrival = arrival[i];
+    time = minArrival;
+    cout << "\nGantt Chart: ";
+    while (completed < n) {
+
+        int idx = -1;
+        int bestP = 99999;
+        // it will pick highest priority available for
+        (int i = 0; i < n; i++) {
+            if (done[i] == 0 && arrival[i] <= time) { if
+            (priority[i] < bestP) {
+                bestP = priority[i];
+                idx = i;
+            }
+            }
+        }
+        if (idx == -1) {
+            time++;
+            continue;
+        }
+        start[idx] = time;
+        time = time + burst[idx];
+        finish[idx] = time;
+        waiting[idx] = start[idx] - arrival[idx];
+        tat[idx] = finish[idx] - arrival[idx];
+        done[idx] = 1;
+        completed++;
+        cout << "| " << pid[idx] << " ";
+    }
+    cout << "|\n";
+    double wsum = 0, tsum = 0;
+    cout << "\nProcess\tAT\tBT\tP\tWT\tTAT\n";
+    for (int i = 0; i < n; i++) {
+        cout << pid[i] << "\t" << arrival[i] << "\t" << burst[i] << "\t" <<
+        priority[i] << "\t" << waiting[i] << "\t" << tat[i] << endl;
+        wsum += waiting[i];
+        tsum += tat[i];
+    }
+    cout << "\nAverage Waiting Time: " << wsum / n;
+
+    cout << "\nAverage Turnaround Time: " << tsum / n;
+    return 0;
+}
